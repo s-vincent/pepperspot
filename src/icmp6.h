e4134a7af85369ad930f661788ca2b84e094d3c3
vancon <vancon@d99f7ed1-dc7b-4e29-a9ea-e33d517c225d>
2008-06-15 08:42:41 +0000

    Import the project

---
+++ src/icmp6.h
@@ -0,0 +1,77 @@
+/*
+ * PepperSpot -- The Next Generation Captive Portal
+ * Copyright (C) 2008,  Thibault Vançon and Sebastien Vincent
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact: thibault.vancon@eturs.u-strasbg.fr
+ *          vincent@lsiit.u-strasbg.fr
+ *
+ * You can find a Copy of this license in the LICENSE file
+ */
+
+/* $Id: icmp6.h 1.17 06/05/07 21:52:43+03:00 anttit@tcs.hut.fi $ */
+
+#ifndef __ICMP6_H__
+#define __ICMP6_H__ 1
+
+#include <netinet/icmp6.h>
+
+struct icmp6_handler {
+	struct icmp6_handler *next;
+	void (* recv)(const struct icmp6_hdr *ih, 
+		      ssize_t len, 
+		      const struct in6_addr *src,
+		      const struct in6_addr *dst,
+		      int iif,
+		      int hoplimit);
+};
+
+struct sock 
+{
+   pthread_mutex_t send_mutex; 
+  int fd;
+};
+
+
+
+#define ICMP6_MAIN_SOCK -1
+
+int if_mc_group(int sock, int ifindex, const struct in6_addr *mc_addr,
+		int cmd);
+
+void icmp6_handler_reg(uint8_t type, struct icmp6_handler *handler);
+void icmp6_handler_dereg(uint8_t type, struct icmp6_handler *handler);
+
+int icmp6_init(void);
+void icmp6_cleanup(void);
+
+int icmp6_send(int oif, uint8_t hoplimit, const struct in6_addr *src,
+	       const struct in6_addr *dst, struct iovec *datav, size_t iovlen);
+
+ssize_t icmp6_recv(int sock, unsigned char *msg, size_t msglen,
+		   struct sockaddr_in6 *addr, struct in6_pktinfo *pkt_info,
+		   int *hoplimit);
+
+void *icmp6_create(struct iovec *iov, uint8_t type, uint8_t code);
+
+struct ip6_hdr;
+
+int icmp6_parse_data(struct ip6_hdr *ip6h, unsigned int len, 
+		     struct in6_addr **lhoa, struct in6_addr **rhoa);
+
+void* icmp6_listen(void* arg);
+
+#endif
---
+++ src/md5.h
@@ -0,0 +1,51 @@
+/*
+ * PepperSpot -- The Next Generation Captive Portal
+ * Copyright (C) 2008,  Thibault Vançon and Sebastien Vincent
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact: thibault.vancon@eturs.u-strasbg.fr
+ *          vincent@lsiit.u-strasbg.fr
+ *
+ * You can find a Copy of this license in the LICENSE file
+ */
+
+#ifndef MD5_H
+#define MD5_H
+
+#ifdef __alpha
+typedef unsigned int uint32;
+#else
+typedef unsigned long uint32;
+#endif
+
+struct MD5Context {
+	uint32 buf[4];
+	uint32 bits[2];
+	unsigned char in[64];
+};
+
+void MD5Init(struct MD5Context *context);
+void MD5Update(struct MD5Context *context, unsigned char const *buf,
+	       unsigned len);
+void MD5Final(unsigned char digest[16], struct MD5Context *context);
+void MD5Transform(uint32 buf[4], uint32 const in[16]);
+
+/*
+ * This is needed to make RSAREF happy on some MS-DOS compilers.
+ */
+typedef struct MD5Context MD5_CTX;
+
+#endif /* !MD5_H */
